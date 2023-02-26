3cb0896f2c4be4a668f1f924628da5e5ac39a484
mirza@Regulus <exquisitemirza@student.ub.ac.id>
2023-02-26 17:09:31 +0700

    ``feat``: Add Pasien Class with its properties

--- SK01_13_225150707111001_GhatfanEmeryRazan.java
+++ SK01_13_225150707111001_GhatfanEmeryRazan.java
@@ -2,9 +2,39 @@ public class SK01_13_225150707111001_GhatfanEmeryRazan {
 
 }
 
+class Pasien {
+    private String name;
+    private JenisLayanan jenisLayanan;
+
+    public Pasien(String name, JenisLayanan jenisLayanan) {
+        this.name = name;
+        this.jenisLayanan = jenisLayanan;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public JenisLayanan getJenisLayanan() {
+        return jenisLayanan;
+    }
+
+    public void setJenisLayanan(JenisLayanan jenisLayanan) {
+        this.jenisLayanan = jenisLayanan;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return ((Pasien) obj).jenisLayanan == this.jenisLayanan;
+    }
+}
+
 enum JenisLayanan {
     VVIP,
     VIP,
     BIASA
 }
-
